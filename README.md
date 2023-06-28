3cb44d8ba39a88a2ad6b4d13a298e8b5dff50c79
Ruedarosa <138029061+Ruedarosa@users.noreply.github.com>
2023-06-28 11:53:45 -0300

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,24 @@
+
+> Open this page at [https://ruedarosa.github.io/maqueen-sensor/](https://ruedarosa.github.io/maqueen-sensor/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/ruedarosa/maqueen-sensor** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://makecode.microbit.org/](https://makecode.microbit.org/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/ruedarosa/maqueen-sensor** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/microbit
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,35 @@
+let item = false
+let item2: neopixel.Strip = null
+let red = 0
+let green = 0
+let BLUE = 0
+basic.forever(function () {
+    if (maqueen.sensor(PingUnit.Centimeters) < 35 && maqueen.sensor(PingUnit.Centimeters) != 0) {
+        item = Math.randomBoolean()
+        if (item == true) {
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 0)
+            basic.showNumber(maqueen.sensor(PingUnit.Centimeters))
+            basic.pause(800)
+        }
+        if (item == false) {
+            maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 0)
+            maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+            basic.showNumber(maqueen.sensor(PingUnit.Centimeters))
+            basic.pause(800)
+        }
+    } else {
+        maqueen.MotorRun(maqueen.aMotors.M1, maqueen.Dir.CW, 255)
+        maqueen.MotorRun(maqueen.aMotors.M2, maqueen.Dir.CW, 255)
+        item2 = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
+        red = 0
+        green = 0
+        BLUE = 255
+        for (let index = 0; index < 255; index++) {
+            green += 1
+            red += -1
+            item2.showColor(neopixel.rgb(red, green, BLUE))
+            basic.pause(1000)
+        }
+    }
+})
